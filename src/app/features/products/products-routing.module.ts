97e800e5069b05d038a4f25df9f5c1d78d2e683a
Cipher-13 <aihab6087@gmail.com>
2026-01-31 21:14:52 +0200

    products-2

--- src/app/features/products/products-routing.module.ts
+++ src/app/features/products/products-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import { P2Component } from './components/p2/p2.component';
 
 const routes: Routes = [
-  { path: '', component: ProductsComponent }
+  { path: '', component: ProductsComponent },
+  { path: 'p2', component: P2Component },
 ];
 
 @NgModule({
