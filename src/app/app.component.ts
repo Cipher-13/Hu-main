f3817c718a15e91a3bdf6bed517216f41e590695
Cipher-13 <aihab6087@gmail.com>
2026-01-31 12:44:45 +0200

    nav+footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'humain';
+
+    isDropdownOpen = false;
+
+  onDropdownState(state: boolean) {
+    this.isDropdownOpen = state;
+  }
 }
--- src/app/layouts/nav-bar/nav-bar.component.ts
+++ src/app/layouts/nav-bar/nav-bar.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+
+  @Output() dropdownState = new EventEmitter<boolean>();
+
+  ngAfterViewInit(): void {
+
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+
+      dropdown.addEventListener('show.bs.dropdown', () => {
+        this.dropdownState.emit(true);
+      });
+
+      dropdown.addEventListener('hide.bs.dropdown', () => {
+        this.dropdownState.emit(false);
+      });
+
+    });
+  }
 }
