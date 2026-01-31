4373e62fdbf10198caac7dc2ebe03c306079a3a2
Cipher-13 <aihab6087@gmail.com>
2026-01-31 20:27:26 +0200

    products-1

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+interface Card {
+  title: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-products',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-
+ cards: Card[] = [
+    { title: 'Card 1', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 2', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 3', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 4', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 5', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 6', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 7', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 8', image: '../../../assets/images/OIP.jfif' },
+    { title: 'Card 9', image: '../../../assets/images/OIP.jfif' },
+  ];
 }
